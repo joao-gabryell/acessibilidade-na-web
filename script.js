9591f015c655ec73cfbebe9c15d84d63d9e88074
joao-gabryell <163905001+joao-gabryell@users.noreply.github.com>
2025-03-11 16:04:21 -0300

    1604

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded'),function(){
+    const aumentarfonteBotao=Document.getElementById('aumentar-fonte');
+
+    let tamanhoAtulFonte=1;
+    aumentarfonteBota.addEventListener('click',function(){
+        tamanhoAtual+= 0,1;
+        document.body.style.fontSize = `${tamanhoAtulFonte}rem`
+})
+
+}
