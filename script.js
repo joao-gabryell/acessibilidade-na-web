2fb8a430228ce91837204dac55b707f5d85ef560
joao-gabryell <163905001+joao-gabryell@users.noreply.github.com>
2025-03-18 15:25:10 -0300

    0318

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded'),function(){
     const aumentarfonteBotao=Document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao=Document.getElementById('diminuir-fonte');
 
-    let tamanhoAtulFonte=1;
+    let tamanhoAtualFonte=1;
+
     aumentarfonteBotao.addEventListener('click',function(){
         tamanhoAtual+= 0,1;
-        document.body.style.fontSize = `${tamanhoAtulFonte}rem`
-diminuiFonteBotao.addEventListener('click',function(){
-    tamanhoAtulFonte -=0,1;
-    document.body.style.fontSize=`${tamanhoAtulFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
-})
+ diminuiFonteBotao.addEventListener('click',function(){
+    tamanhoAtualFonte -=0,1;
+    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+
+ })
 
 }
